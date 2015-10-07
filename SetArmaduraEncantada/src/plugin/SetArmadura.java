eb5167da6bb783d9d9a969211c39dc6871563727
Key2Board12 <captaincoca365@gmail.com>
2015-10-07 18:32:32 +0200

    Armadura Plugin Mejorada
    OP= Diamond
    NoOP = Leather

--- SetArmaduraEncantada/src/plugin/SetArmadura.java
+++ SetArmaduraEncantada/src/plugin/SetArmadura.java
@@ -26,6 +26,59 @@ public class SetArmadura extends JavaPlugin implements Listener{
 
 		Player p = e.getPlayer();
 
+		//si p es admin dar armadura
+		if(p.isOp()){
+			darArmadura(p);
+		}else{
+			darArmaduraMala(p);
+		}
+			
+		
+	}
+
+
+	private void darArmaduraMala(Player p) {
+		PlayerInventory inv =p.getInventory();
+		
+		ItemStack espadas = new ItemStack(Material.STONE_SWORD);
+		ItemMeta espadasMeta = espadas.getItemMeta();
+		espadasMeta.setDisplayName(ChatColor.GRAY + "Espada De Principio ");
+		espadas.setItemMeta(espadasMeta);
+		inv.addItem(espadas);
+		
+		ItemStack pechera = new ItemStack(Material.LEATHER_CHESTPLATE);
+		ItemMeta pecheraMeta = pechera.getItemMeta();
+		pecheraMeta.setDisplayName(ChatColor.GRAY + "Pechera De Principio " + p.getName());
+		pechera.setItemMeta(pecheraMeta);
+		inv.setChestplate(pechera);
+		
+		
+		ItemStack casco = new ItemStack(Material.LEATHER_HELMET);
+		ItemMeta cascoMeta = casco.getItemMeta();
+		cascoMeta.setDisplayName(ChatColor.GRAY + "Casco De Principio " + p.getName());
+		casco.setItemMeta(cascoMeta);
+		inv.setHelmet(casco);
+		
+		
+		ItemStack botas= new ItemStack(Material.LEATHER_BOOTS);
+		ItemMeta botasMeta = botas.getItemMeta();
+		botasMeta.setDisplayName(ChatColor.GRAY + "Botas De Principio " + p.getName());
+		botas.setItemMeta(botasMeta);
+		inv.setBoots(botas);
+		
+		ItemStack pantalones= new ItemStack(Material.LEATHER_LEGGINGS);
+		ItemMeta pantalonesMeta = pantalones.getItemMeta();
+		pantalonesMeta.setDisplayName(ChatColor.GRAY+ "Pantalones De Principio " + p.getName());
+		pantalones.setItemMeta(pantalonesMeta);
+		inv.setLeggings(pantalones);
+		
+	
+	
+	
+	}
+
+	public void darArmadura(Player p){
+
 		//Obtenemos el inventario del jugador
 		//variable de tipo PlayerInventory de nombre inv
 
@@ -70,15 +123,7 @@ public class SetArmadura extends JavaPlugin implements Listener{
 		botas.setItemMeta(botasMeta);
 		botas.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 4);
 		inv.setBoots(botas);
-
-
 	}
-
-
-
-
-
-
 
 
 
